38348862e7297007566e1b5bc7ac3bf3ebd29036
BangiBono <marcinbanas91@gmail.com>
2022-03-30 18:59:28 +0200

    oh it's uff

---
+++ ex5.py
@@ -0,0 +1,19 @@
+name = 'Marcin'
+age = 30 # not a lie 
+height = 178 # cm
+weight = 70 # kg
+eyes = 'Blue'
+teetth = 'White'
+hair = 'Brown'
+
+print("Let's talk about %s." % name)
+print("He's %d cm tall." % height)
+print("He's %d kg heavy." % weight)
+print("Actually that's not too heavy.")
+print("He's got %s eyess and %s hair." % (eyes, hair))
+print("His teeth are usually %s depending on the coffee." % (teetth))
+
+
+print("If I add %d, %d, and %d I get %d" % (age, height, weight, age + height + weight))
+
+
